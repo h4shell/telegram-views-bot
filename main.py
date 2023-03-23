fd5436684a306630fd3a7096ff4c5efdd2029e5d
h4shell <fw1987@gmail.com>
2023-03-23 13:10:14 +0100

    update

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from threading import Thread, active_count
 from auto_proxy import Proxy
 from telegram import Api
 
-THREADS = 400
+THREADS = 500
 logo = '''
    ~ Telegram Auto Views V2 ~
      ~ github.com/TeaByte ~
@@ -12,7 +12,7 @@ logo = '''
 '''
 
 print(logo)
-channel, post = input_loader()
+channel, post = input_loader("")
 http, socks4, socks5 = auto_loader()
 
 auto = Proxy(http_sources=http, socks4_sources=socks4, socks5_sources=socks5)
@@ -26,12 +26,14 @@ def start():
     threads = []
     auto.init()
     for proxy_type, proxy in auto.proxies:
-        while active_count() > THREADS: swait(0.05)
+        while active_count() > THREADS:
+            swait(0.05)
         thread = Thread(target=api.send_view, args=(proxy, proxy_type))
         threads.append(thread)
         thread.start()
     for t in threads:
         t.join()
         start()
+
 
 start()
--- utilitys.py
+++ utilitys.py
@@ -4,28 +4,30 @@ from re import search
 
 
 error_file = open('errors.txt', 'a+', encoding='utf-8')
-logger = lambda error: error_file.write(f'{error}\n')
+def logger(error): return error_file.write(f'{error}\n')
 
 
 def auto_loader():
-    try: 
+    try:
         cfg = ConfigParser(interpolation=None)
         cfg.read("config.ini", encoding="utf-8")
         return (
-            cfg["HTTP"].get("Sources").splitlines(), 
-            cfg["SOCKS4"].get("Sources").splitlines(), 
+            cfg["HTTP"].get("Sources").splitlines(),
+            cfg["SOCKS4"].get("Sources").splitlines(),
             cfg["SOCKS5"].get("Sources").splitlines()
         )
-    except KeyError: 
+    except KeyError:
         print(' [ Error ] config.ini not found!')
-        swait(3);quit()
+        swait(3)
+        quit()
 
 
-def input_loader():
-    url_input = search(r'(https?:\/\/t\.me\/)?([^/]+)/(\d+)', input(' [ INPUT ] Enter Post URL: '))
-    if url_input: 
+def input_loader(url):
+    url_input = search(r'(https?:\/\/t\.me\/)?([^/]+)/(\d+)', url)
+    if url_input:
         _, channel, post = url_input.groups()
         return channel, post
-    else: 
+    else:
         print(' [ ERROR ] Channel Or Post Not Found!')
-        swait(3);quit()
+        swait(3)
+        quit()
